fdf1a43b2a6f23bf47956ef9cf69bfe3ab163840
morbey <eduardomorbey@gmail.com>
2025-10-19 22:05:21 +0100

    group usage working

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    delta_time = 0
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -26,11 +31,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.draw(screen)
-        player.update(delta_time)
-        pygame.display.flip()
+        updatable.update(dt)
 
-        delta_time = clock.tick(60)/1000
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
